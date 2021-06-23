6bdd7f036de6de03ee97316ac207054b4c783577
Pankaj Dimble <pankajdimble@Pankajs-MacBook-Pro.local>
2021-06-23 14:30:04 -0700

    header design

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import './header.css'
 const Header = () =>{
-
-    return(
-        <div className ="header-container">
-            <h1>Note App</h1>
-        </div>
-        
-    )
+  return(
+    <div className ="header-container">
+      <h1>Note App</h1>
+    </div>
+      
+  )
 }
 export default Header

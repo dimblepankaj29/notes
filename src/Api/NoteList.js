0fbd6803878fbb9d56ce09e76d761fe19ff24c2a
Pankaj Dimble <pankajdimble@Pankajs-MacBook-Pro.local>
2021-06-23 14:31:02 -0700

    API build to get data

--- src/Api/NoteList.js
+++ src/Api/NoteList.js
@@ -1,15 +1,10 @@
 import {NoteListData} from '../constant/constant'
 
 export const getList=()=>{
-    const res = new Promise (async (resolve, reject)=>{
-        // console.log(NoteListData)
-        const data =  NoteListData
-         resolve (data)
+   return new Promise (async (resolve, reject)=>{
+         resolve (NoteListData)
     })
-    res.then(function (data){
-        // console.log(data)
-         return data
-    })
+    
 }
 
                    

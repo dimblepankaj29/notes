050488ae340ba4930475b297eafbcfb794600c20
Pankaj Dimble <pankajdimble@Pankajs-MacBook-Pro.local>
2021-06-23 14:29:47 -0700

    note list comps

--- src/NoteList/NodeList.js
+++ src/NoteList/NodeList.js
@@ -1,32 +1,50 @@
 import React, {useState, useEffect} from 'react'
-// import {NoteListData} from '../constant/constant'
 import NoteItems from '../NoteItems/NoteItems'
 import {getList} from '../Api/NoteList'
 import './NoteList.css'
 
 const NoteList = () =>{
-    const [noteItems, setNoteItems] = useState([])
-    const [noteData, setNoteData] = useState ([])
-    const handleNoteClick = (noteId)=> {
-      setNoteItems(noteData.find(note => note.noteId === noteId).notes)
-    }
+  const [noteItems, setNoteItems] = useState([])
+  const [noteData, setNoteData] = useState ([])
+  const [selectNote, setSelectedNote] = useState("")
 
-    useEffect (()=>{
-      console.log("getList",getList())
-        setNoteData(getList())
-    },[])
-    return(
-    <>
-      <ul className ="note-container">
-        {noteData?.length >0 &&  noteData.map(note => 
-            <li className ="note-item" key = {note.noteId} 
-            onClick= {() => handleNoteClick(note.noteId)}>{note.noteTitle}</li>  
-        )}
-      </ul>
-      <NoteItems noteItems = {noteItems} />
-    </>
-   
-    )
+  useEffect (()=>{
+    // Get note list from API call
+    getList().then(res =>{
+      setNoteData(res)
+    })
+  },[])
+
+  useEffect (()=>{
+    selectNote && setNoteItems(noteData.find(note => note.noteId === selectNote))
+  },[noteData])
+  
+  const addNote = (note) =>{
+    noteData.forEach(r => {
+      if (r.noteId === selectNote){
+          r.notes.push({ noteItemId: r.notes.length+1, text: note})
+      }
+    })
+      setNoteData(noteData)
+  }
+
+  const handleNoteClick = (noteId)=> {
+    setSelectedNote(noteId)
+    setNoteItems(noteData.find(note => note.noteId === noteId))
+  }
+  
+  return(
+  <>
+    <ul className ="note-container">
+      {noteData?.length >0 &&  noteData.map(note => 
+          <li className = {selectNote === note.noteId ? "note-item-active": "note-item"} key = {note.noteId} 
+          onClick= {() => handleNoteClick(note.noteId)}>{note.noteTitle}</li>  
+      )}
+    </ul>
+    <NoteItems noteItems = {noteItems} addNote = {addNote} />
+  </>
+  
+  )
 }
 
 export default NoteList

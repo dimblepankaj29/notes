716f9e39145d1a4fcca916ad92a14eaefd9c7909
Pankaj Dimble <pankajdimble@Pankajs-MacBook-Pro.local>
2021-06-23 14:29:24 -0700

    Note Item list comps

--- src/NoteItems/NoteItems.js
+++ src/NoteItems/NoteItems.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import NoteInput from '../NoteInput/NoteInput'
 import './NoteItems.css'
 const NoteItems = (props) =>{
-    const {noteItems} = props
-    console.log(noteItems)
+    const {noteItems,addNote} = props
+    const [note, setNote] = useState('')
+
+    const handleOnClick = ()=>{
+      addNote(note)
+      setNote('')
+    }
     return (
-       <> 
-         <ul className = "note-item-container">
-             {noteItems.map(li => <li className = "note-list-item">{li.text}</li>)}
+       <div className =" note-item-container"> 
+        <div className="noteInput-block">
+          <NoteInput note = {note} setNote = {setNote}/>
+          <button onClick={ handleOnClick } disabled = {!note && !noteItems?.noteId}>Add</button>
+         </div>
+         {Object.keys(noteItems).length ===0 && <p><strong>Please select Note to see the Note list</strong></p>}
+         <ul >
+             {noteItems?.notes?.map(li => <li className = "note-list-item">{li.text}</li>)}
          </ul>
-       </> 
+       </div> 
     )
 }
 
